13dbba36016eefcb07fcd714044215eac5c7b430
Yasoii <buyonline@abv.bg>
2020-01-29 20:41:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie list</title>
+</head>
+<body>
+    <div class="content">
+<h1>Favourite Movies</h1>
+
+<ul>
+<li>The Matrix </li>
+<li>Gudzila </li>
+<li>Man of steel </li>
+</ul>
+
+
+</body>
+</html>
