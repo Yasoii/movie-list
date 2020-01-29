ba125d4e41aecb0b6ca07733946a3b1d53734d90
Yasoii <buyonline@abv.bg>
2020-01-29 21:51:58 +0200

    done

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movie list</title>
-    <link rel="Stylesheet" href="style.css">
+    <title>Movie List</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="content">
-<h1>Favourite Movies</h1>
-
-<ul>
-<li>The Matrix </li>
-<li>Gudzila </li>
-<li>Man of steel </li>
+        <h1>Favorite Movies</h1>
+        <ul>
+        <ul class="movie-list">
+            <li>The Matrix</li>
+            <li>The Case for Christ</li>
+            <li>Man of Steel</li>
 </ul>
 
 
